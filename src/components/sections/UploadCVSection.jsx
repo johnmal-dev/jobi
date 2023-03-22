3908b0bcecf182438869ec83b75543690ae33645
John Malapit <johncmalapit@gmail.com>
2023-03-22 14:05:07 -0400

    html markup and svg for UploadCV section

--- src/components/sections/UploadCVSection.jsx
+++ src/components/sections/UploadCVSection.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const UploadCVSection = () => {
-  return <div>UploadCVSection</div>
+  return (
+    <div>
+      <h3>
+        Get your <span>Matched Jobs</span> in a few minutes.
+      </h3>
+      <p>
+        Find your dream job & earn from world leading brands. Upload your CV
+        now.
+      </p>
+      <button>Upload your CV</button>
+    </div>
+  )
 }
 
 export default UploadCVSection
