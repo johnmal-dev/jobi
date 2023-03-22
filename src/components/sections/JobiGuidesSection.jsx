d433f91788c4e45e8f99ec68b552f525daa503a8
John Malapit <johncmalapit@gmail.com>
2023-03-22 14:01:34 -0400

    html markup for JobiGuides section

--- src/components/sections/JobiGuidesSection.jsx
+++ src/components/sections/JobiGuidesSection.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
 
+const articles = [
+  {
+    image: '',
+    category: 'Developer, Code',
+    title: 'Print, publishing qui visual layout mockups',
+    author: 'Rashed Kabir',
+  },
+  {
+    image: '',
+    category: 'Design, Art',
+    title: "Designer's checklist for every UX/UI project.",
+    author: 'Julie Margot',
+  },
+  {
+    image: '',
+    category: 'Solution, Work',
+    title: 'Make more productive work flow in fewer steps.',
+    author: 'Jubayer Al Hasan',
+  },
+]
+
 const JobiGuidesSection = () => {
-  return <div>JobiGuidesSection</div>
+  return (
+    <div>
+      {/* Top */}
+      <div>
+        <h3>Jobi Guides</h3>
+        <Link to="/">Explore More</Link>
+      </div>
+      {/* Articles */}
+      <div>
+        {articles.map((article, index) => {
+          ;<div key={index}>
+            <div>
+              <img src={article.image} alt="" />
+            </div>
+            <div>{article.category}</div>
+            <div>{article.title}</div>
+            <div>
+              By <span>{article.author}</span>
+            </div>
+          </div>
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default JobiGuidesSection
