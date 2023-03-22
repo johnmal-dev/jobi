f97c920d45317b6b4648e16a2f2e55fc989a5875
John Malapit <johncmalapit@gmail.com>
2023-03-22 15:44:44 -0400

    initial styling for collaboration section

--- src/components/sections/CollaborationSection.jsx
+++ src/components/sections/CollaborationSection.jsx
@@ -5,21 +5,33 @@ import art2 from '../../assets/job-categories-art.svg'
 
 const CollaborationSection = () => {
   return (
-    <div>
-      {/* Left */}
-      <div>
-        <img src={art1} alt="" />
-        <img src={art2} alt="" />
-      </div>
-      {/* Right */}
-      <div>
-        <div>Top Brand</div>
-        <h3>Collaboration with Top Brands.</h3>
-        <p>
-          We collaborate with a number of top tier companies on imagining the
-          future of work, have a look.
-        </p>
-        <Link to="/">Learn More</Link>
+    <div className="container mx-auto my-36">
+      <div className="flex items-center justify-center gap-32">
+        {/* Left */}
+        <div className="relative flex-1">
+          <img src={art1} alt="" />
+          <img
+            src={art2}
+            alt=""
+            className="absolute bottom-1/2 right-0 -translate-x-3/4 translate-y-full"
+          />
+        </div>
+        {/* Right */}
+        <div className="flex flex-1 flex-col gap-12">
+          <div className="text-xs uppercase text-black text-opacity-20">
+            Top Brand
+          </div>
+          <h3 className="font-eb-garamond text-6xl text-primary-700">
+            Collaboration with Top Brands.
+          </h3>
+          <p className="text-2xl leading-10 text-black text-opacity-70">
+            We collaborate with a number of top tier companies on imagining the
+            future of work, have a look.
+          </p>
+          <Link to="/" className="font-bold">
+            Learn More
+          </Link>
+        </div>
       </div>
     </div>
   )
