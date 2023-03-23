797f197bf3dbb02fac4101eb3dc062c87e7db072
John Malapit <johncmalapit@gmail.com>
2023-03-23 01:57:58 -0400

    update header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,41 +8,62 @@ const Header = () => {
         {/* Left */}
         <div className="flex items-center justify-start">
           {/* Jobi Icon */}
-          <div className="font-heading text-4xl before:pr-2 before:content-jobi-icon">
+          <Link
+            to="/"
+            className="block font-heading text-4xl transition ease-in-out before:pr-2 before:content-jobi-icon hover:text-primary-300 focus:text-primary-300"
+          >
             jobi
-          </div>
+          </Link>
           {/* Category Button */}
-          <button className="mx-6 rounded-full bg-secondary-100 bg-opacity-10 px-4 py-1.5 text-primary-300 before:pr-2 before:content-category-icon">
+          <button className="mx-6 rounded-full bg-secondary-100 bg-opacity-10 px-4 py-1.5 text-primary-300 transition ease-in-out before:pr-2 before:content-category-icon hover:text-primary-100 focus:text-primary-100">
             Category
           </button>
           {/* Menu */}
           <ul className="mx-4 flex items-center justify-center gap-6">
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" className="link link-white">
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/jobs">Jobs</Link>
+              <Link to="/jobs" className="link link-white">
+                Jobs
+              </Link>
             </li>
             <li>
-              <Link to="/">Explore</Link>
+              <Link to="/" className="link link-white">
+                Explore
+              </Link>
             </li>
             <li>
-              <Link to="/">Contact</Link>
+              <Link to="/" className="link link-white">
+                Contact
+              </Link>
             </li>
             <li>
-              <Link to="/">Pages</Link>
+              <Link to="/" className="link link-white">
+                Pages
+              </Link>
             </li>
           </ul>
         </div>
         {/* Right */}
         <div className="flex items-center justify-center">
-          <div className="border-r-2 border-secondary-100 border-opacity-25 px-4">
+          <Link
+            to="/"
+            className="link-white link mx-2 block border-r border-secondary-100 border-opacity-25 pr-4"
+          >
             Post Job
-          </div>
-          <div className="px-4 text-primary-300">Login</div>
-          <div className="rounded-full bg-primary-300 px-6 py-2 text-primary-700">
+          </Link>
+          <Link
+            to="/"
+            className="link link-green mx-2 block px-2 text-primary-300"
+          >
+            Login
+          </Link>
+          <Link to="/" className="btn btn-primary block">
             Hire Top Talents
-          </div>
+          </Link>
         </div>
         {/* Header Art */}
         <div className="absolute left-2/3 -translate-x-36 before:content-header-art"></div>
