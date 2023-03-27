6578c493df7ec7932292d732b7519aa0a4993866
John Malapit <johncmalapit@gmail.com>
2023-03-27 13:06:13 -0400

    update footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,177 +1,157 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { FaFacebookF, FaInstagram, FaPinterestP } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
-const footerCategories = [
-  {
-    name: 'Products',
-    links: [
-      {
-        text: 'Take the tour',
-        url: '',
-      },
-      {
-        text: 'Live chat',
-        url: '',
-      },
-      {
-        text: 'Self-service',
-        url: '',
-      },
-      {
-        text: 'Mobile',
-        url: '',
-      },
-      {
-        text: 'Collaboration',
-        url: '',
-      },
-      {
-        text: 'Reviews',
-        url: '',
-      },
-    ],
-  },
-  {
-    name: 'Links',
-    links: [
-      {
-        text: 'Pricing',
-        url: '',
-      },
-      {
-        text: 'About us',
-        url: '',
-      },
-      {
-        text: 'Careers',
-        url: '',
-      },
-      {
-        text: 'Blog',
-        url: '',
-      },
-    ],
-  },
-  {
-    name: 'Legal',
-    links: [
-      {
-        text: 'Terms of use',
-        url: '',
-      },
-      {
-        text: 'Terms & Conditions',
-        url: '',
-      },
-      {
-        text: 'Privacy',
-        url: '',
-      },
-      {
-        text: 'Cookie Policy',
-        url: '',
-      },
-    ],
-  },
-]
+const navigation = {
+  products: [
+    { name: 'Take the tour', href: '#' },
+    { name: 'Live chat', href: '#' },
+    { name: 'Self-service', href: '#' },
+    { name: 'Mobile', href: '#' },
+    { name: 'Collaboration', href: '#' },
+    { name: 'Reviews', href: '#' },
+  ],
+  links: [
+    { name: 'Pricing', href: '#' },
+    { name: 'About us', href: '#' },
+    { name: 'Careers', href: '#' },
+    { name: 'Blog', href: '#' },
+  ],
+  legal: [
+    { name: 'Terms of use', href: '#' },
+    { name: 'Terms & Conditions', href: '#' },
+    { name: 'Privacy', href: '#' },
+    { name: 'Cookie Policy', href: '#' },
+  ],
+  social: [
+    {
+      name: 'Facebook',
+      href: '#',
+      icon: (props) => <FaFacebookF />,
+    },
+    {
+      name: 'Instagram',
+      href: '#',
+      icon: (props) => <FaInstagram />,
+    },
+    {
+      name: 'Pinterest',
+      href: '#',
+      icon: (props) => <FaPinterestP />,
+    },
+  ],
+}
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div className="container">
-      {/* Top */}
-      <div className="flex items-center justify-between border-b border-secondary-400 pb-16">
-        {/* Text */}
-        <div>
-          <h3 className="font-heading text-5xl">Most complete job portal.</h3>
-          <p className="mt-2 text-xl font-normal text-black text-opacity-60">
-            Signup and start find your job or talents.
-          </p>
-        </div>
-        {/* Buttons */}
-        <div>
-          <button className="mr-7 rounded-md border border-primary-700 py-4 px-7 text-primary-700">
-            Looking for job?
-          </button>
-          <button className="rounded-md border border-primary-300 bg-primary-300 py-4 px-7 text-primary-700">
-            Post a job
-          </button>
-        </div>
-      </div>
-      {/* Middle */}
-      <div className="my-20 grid grid-cols-5">
-        {/* Jobi Icon */}
-        <div className="font-heading text-4xl before:pr-2 before:content-jobi-icon">
-          jobi
-        </div>
-        {footerCategories.map((category, index) => {
-          return (
-            <div key={index}>
-              <h4 className="text-2xl font-bold">{category.name}</h4>
-              <ul className="mt-6">
-                {category.links.map((link, index) => {
-                  return (
-                    <li key={index} className="mt-6">
-                      <Link
-                        to={link.url}
-                        className="text-black text-opacity-70"
+    <footer aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="lg:pt-18 container px-6 pb-4 sm:pt-12 lg:px-8">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div className="grid grid-cols-2 gap-8 xl:col-span-2">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div className="font-heading text-4xl before:pr-2 before:content-jobi-icon">
+                jobi
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                  Products
+                </h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.products.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-black hover:text-primary-500"
                       >
-                        {link.text}
-                      </Link>
+                        {item.name}
+                      </a>
                     </li>
-                  )
-                })}
-              </ul>
+                  ))}
+                </ul>
+              </div>
             </div>
-          )
-        })}
-        <div>
-          <h4 className="text-2xl font-bold">Newsletter</h4>
-          <div className="mt-6">Join & get important news regularly</div>
-          <form className="mt-6">
-            <div className="flex rounded-lg bg-secondary-100 p-2 shadow-sm ring-1 ring-inset ring-secondary-100 focus-within:ring-2 focus-within:ring-primary-300">
-              <input
-                type="text"
-                placeholder="Enter your email"
-                className="flex-1 border-none bg-transparent pr-0 placeholder-black placeholder-opacity-25 focus:ring-0"
-              />
-              <button className="rounded-md bg-primary-700 p-2 text-white focus:border-none focus:outline-none focus:ring-2">
-                Send
-              </button>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                  Links
+                </h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.links.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-black hover:text-primary-500"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                  Legal
+                </h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.legal.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-black hover:text-primary-500"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </form>
-          <div className="mt-4 text-sm text-secondary-900 text-opacity-50">
-            We only send interesting and relevent emails.
+          </div>
+          <div className="mt-10 xl:mt-0">
+            <h4 className="text-2xl font-bold">Newsletter</h4>
+            <div className="mt-6">Join & get important news regularly</div>
+            <form className="mt-6">
+              <div className="flex rounded-lg bg-secondary-100 p-2 shadow-sm ring-1 ring-inset ring-secondary-100 focus-within:ring-2 focus-within:ring-primary-300">
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  className="w-20 flex-1 border-none bg-transparent pr-0 placeholder-black placeholder-opacity-25 focus:ring-0 xs:w-full"
+                />
+                <button className="rounded-md bg-primary-700 p-2 text-white focus:border-none focus:outline-none focus:ring-2">
+                  Send
+                </button>
+              </div>
+            </form>
+            <div className="mt-4 text-sm text-secondary-900 text-opacity-50">
+              We only send interesting and relevent emails.
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-4 pt-12 lg:grid-cols-3 lg:gap-0">
+          <div className="space-x-6 text-center font-bold lg:text-left">
+            <Link to="/" className="text-black hover:text-primary-500">
+              Privacy & Terms.
+            </Link>
+            <Link to="/">Contact Us</Link>
+          </div>
+          <div className="text-center">Copyright @2023 jobi inc.</div>
+          <div className="flex items-center justify-center gap-4 lg:justify-end">
+            {navigation.social.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-black hover:text-primary-500"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
           </div>
         </div>
       </div>
-      {/* Bottom */}
-      <div className="grid grid-cols-3">
-        <div className="space-x-6 text-left font-bold">
-          <Link to="/">Privacy & Terms.</Link>
-          <Link to="/">Contact Us</Link>
-        </div>
-        <div className="text-center">Copyright @2023 jobi inc.</div>
-        <ul className="justify-right ml-auto flex items-center gap-4">
-          <li>
-            <Link to="/">
-              <FaFacebookF />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <FaInstagram />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <FaPinterestP />
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </footer>
   )
 }
-
-export default Footer
